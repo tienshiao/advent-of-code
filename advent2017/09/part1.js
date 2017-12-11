615a2d333874f3e1fe67ed0a1e495c9e65f134a4
Tienshiao Ma <tma@isbx.com>
2017-12-10 23:34:10 -0800

    add day 9

---
+++ advent2017/09/part1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const string = fs.readFileSync('input1', 'utf8').trim();
+//const string = '{{<!!>},{<!!>},{<!!>},{<!!>}}';
+
+const parts = string.split('');
+
+let count = 0;
+let score = 0;
+let stack = [];
+let garbage = false;
+while (parts.length > 0) {
+  const c = parts.shift();
+
+  if (garbage) {
+    if (c =='!') {
+      parts.shift();
+    } else if (c == '>') {
+      garbage = false;
+    }
+  } else {
+    if (c == '{') {
+      stack.push(c);
+    } else if (c == '}') {
+      score += stack.length;
+      stack.pop();
+      count++;
+    } else if (c == '<') {
+      garbage = true;
+    }
+  }
+}
+
+console.log(count);
+console.log(score);
